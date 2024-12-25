22ef6655e737e96d888ba2df9e58abf117052411
SamMee <sammeepay@hotmail.com>
2024-12-25 22:07:58 +0700

    Removed player damage log (#8894)
    Removed player damage log (#8894)
    It has been confirmed that players that log out stay in the damage log of the monsters, that they have attacked.
    Once the monster dies their online status is being checked and players that are offline are ignored at that point.
    This offline check is already implemented in rAthena additionally, since the player damage log seem to only have been used for MVPs.

--- src/map/clif.cpp
+++ src/map/clif.cpp
@@ -11401,7 +11401,6 @@ void clif_parse_QuitGame(int32 fd, map_session_data *sd)
 	if( !sd->sc.getSCE(SC_CLOAKING) && !sd->sc.getSCE(SC_HIDING) && !sd->sc.getSCE(SC_CHASEWALK) && !sd->sc.getSCE(SC_CLOAKINGEXCEED) && !sd->sc.getSCE(SC_SUHIDE) && !sd->sc.getSCE(SC_NEWMOON) &&
 		(!battle_config.prevent_logout || sd->canlog_tick == 0 || DIFF_TICK(gettick(), sd->canlog_tick) > battle_config.prevent_logout) )
 	{
-		pc_damage_log_clear(sd,0);
 		clif_disconnect_ack(sd, 0);
 		flush_fifo( fd );
 		if( battle_config.drop_connection_on_quit ){
@@ -11787,7 +11786,6 @@ void clif_parse_Restart(int32 fd, map_session_data *sd)
 		if( !sd->sc.getSCE(SC_CLOAKING) && !sd->sc.getSCE(SC_HIDING) && !sd->sc.getSCE(SC_CHASEWALK) && !sd->sc.getSCE(SC_CLOAKINGEXCEED) && !sd->sc.getSCE(SC_SUHIDE) && !sd->sc.getSCE(SC_NEWMOON) &&
 			(!battle_config.prevent_logout || sd->canlog_tick == 0 || DIFF_TICK(gettick(), sd->canlog_tick) > battle_config.prevent_logout) )
 		{	//Send to char-server for character selection.
-			pc_damage_log_clear(sd,0);
 			chrif_charselectreq(sd, session[fd]->client_addr);
 		} else {
 			clif_disconnect_ack(sd, 1);
