c3205523501ab3051545358f5f003b734ff1191a
SamMee <sammeepay@hotmail.com>
2025-03-22 11:52:50 +0700

    Follow up to 6d5b26f (#9185)
    Follow up to 6d5b26f (#9185)
    Small correction of Utan Kid NPC script (#9175)
    Official Signum Crucis Success Chance (#9180)
    Fixed Quick Draw Shot Number of Hits (#9178)
    Restored INF in clif_skillinfo (fixes combo targetting) (#9173)
    Added Illusion Teddy Bear Map Drops (#9166)
    Quick Draw Shot Code Cleanup (#9152)
    Monster RaceGroups corrections (#9158)
    Attack Delays on Cast Begin (#9160)

--- src/map/script_constants.hpp
+++ src/map/script_constants.hpp
@@ -3468,7 +3468,7 @@
 	export_constant(RC_PLAYER_DORAM);
 	export_constant(RC_ALL);
 
-	export_constant(RC2_NONE);
+	export_deprecated_constant(RC2_NONE);	// Mar. 2025
 	export_constant(RC2_GOBLIN);
 	export_constant(RC2_KOBOLD);
 	export_constant(RC2_ORC);
@@ -3507,7 +3507,7 @@
 	export_constant(RC2_EDDA_ARUNAFELTZ);
 	export_constant(RC2_LASAGNA);
 	export_constant(RC2_GLAST_HEIM_ABYSS);
-	export_constant(RC2_MAX);
+	export_deprecated_constant(RC2_MAX);	// Mar. 2025
 
 	/* monster ai */
 	export_constant(MONSTER_TYPE_01);
