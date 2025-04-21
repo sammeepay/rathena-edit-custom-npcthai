d275c298c01c6b04c5486260286b214e8ba9e0f3
SamMee <sammeepay@hotmail.com>
2025-04-21 23:37:23 +0700

    Update clif.cpp

--- src/map/clif.cpp
+++ src/map/clif.cpp
@@ -25890,7 +25890,7 @@ void clif_parse_dynamic_npc( int32 fd, map_session_data* sd ){
  			p->items[slot].identified = st->items_inventory[i].identify;
  			p->items[slot].damaged = st->items_inventory[i].attribute;
  			clif_addcards( &p->items[slot].slot, &st->items_inventory[i] );
- 			clif_add_random_options( p->items[slot].option_data, &st->items_inventory[i] );
+ 			clif_add_random_options( p->items[slot].option_data, st->items_inventory[i] );
  			p->items[slot].location = pc_equippoint_sub( sd, data );
  			p->items[slot].viewSprite = data->look;
  			p->items[slot].refine = st->items_inventory[i].refine;
