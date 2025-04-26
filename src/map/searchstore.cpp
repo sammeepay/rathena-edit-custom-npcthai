e1f4576d6d10329cbf01d13dff5041bbe8a05aac
SamMee <sammeepay@hotmail.com>
2025-04-26 15:07:43 +0700

    update searchstore.cpp and npc disabled
    update searchstore.cpp and npc disabled

--- src/map/searchstore.cpp
+++ src/map/searchstore.cpp
@@ -201,7 +201,7 @@ void searchstore_query(map_session_data& sd, e_searchstore_searchtype type, uint
 	}
 
 	for (auto& itStalls : stall_db){
- 		if(itStalls->vended_id == pl_sd->status.char_id) // skip own shop, if any
+ 		if(itStalls->vended_id == sd.status.char_id) // skip own shop, if any
  			continue;
  
  		if( !stall_searchall(pl_sd, &s, itStalls, type) ) { // exceeded result size
